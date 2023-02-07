ec9392c5cd4e1fd6b19a2d96310a303a9bdf88bb
Rajat Kumar <rajatk12199@gmail.com>
2023-02-08 04:20:06 +1100

    Navigation Bar Javascript Cleanup

--- index.js
+++ index.js
@@ -9,17 +9,14 @@ var scrollPos = window.scrollY;
 // Scroll Event Listener
 window.addEventListener('scroll', function() {
 
-  // Check Between Previous and Current Position
-  if ((document.body.getBoundingClientRect()).top > scrollPos) {
+  // Check Between Previous and Current Position 
+  // && Remove Box Shadow When Top Of Page
+  if ((document.body.getBoundingClientRect()).top > scrollPos 
+  && window.scrollY >= 100) {
     navBar.classList.add('sticky');
   } else {
         navBar.classList.remove('sticky');
     }
-  
-  // Remove Box Shadow When Top Of Page
-  if (window.scrollY < 100) {
-    navBar.classList.remove('sticky')
-  }
 		
 	// Store New Position Value
 	scrollPos = (document.body.getBoundingClientRect()).top;
